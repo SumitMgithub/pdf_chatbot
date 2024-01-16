4b35377641053ebc813b63785c271cc5b362741c
Sumit Malviya <143081763+SumitMgithub@users.noreply.github.com>
2024-01-16 20:13:11 +0530

    Update pdf_cb.py

--- pdf_cb.py
+++ pdf_cb.py
@@ -20,7 +20,7 @@ with st.sidebar:
     - [OpenAI](https://platform.openai.com/docs/models) LLM model
 
     ''')
-    add_vertical_space(5)
+    add_vertical_space(2)
     st.write('Made by Sumit Malviya')
 
 def process_pdf(file_path):
